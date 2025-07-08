1a1f04d7dc9e7d72dc7db661b4f381dba4007395
harshvardhan singh <harshvardhansingh458@protonmail.com>
2025-07-08 19:12:35 +0530

    added os identification for running clear console command

---
+++ utility/Console.go
@@ -0,0 +1,23 @@
+package utility
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+)
+
+func ClearConsole() {
+	switch runtime.GOOS {
+	case "linux", "darwin":
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	case "windows":
+		cmd := exec.Command("cmd", "/c", "cls")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	default:
+		fmt.Print("\033[H\033[2J")
+	}
+}
