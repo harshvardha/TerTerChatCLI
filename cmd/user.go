5f1e573565f0998b838bb5f9f18e4988091d6625
harshvardhan singh <harshvardhansingh458@protonmail.com>
2025-07-25 23:05:34 +0530

    improved error handling

--- cmd/user.go
+++ cmd/user.go
@@ -8,7 +8,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"io"
+	"log"
 	"net/http"
 	"os"
 	"strings"
@@ -17,6 +17,17 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 )
+
+// function to send http request
+func createRequest(verb string, url string, body []byte) (*http.Request, error) {
+	request, err := http.NewRequest(strings.ToUpper(verb), url, bytes.NewBuffer(body))
+	if err != nil {
+		return nil, err
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+	return request, nil
+}
 
 // userCmd represents the user command
 var userCmd = &cobra.Command{
@@ -27,6 +38,9 @@ var userCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("user command called")
 
+		// http client to send request to server
+		httpClient := &http.Client{}
+
 		// checking which flags were set by the user
 		cmd.Flags().Visit(func(f *pflag.Flag) {
 			name := f.Name
@@ -36,71 +50,71 @@ var userCmd = &cobra.Command{
 			case "disconnect":
 				fmt.Println("disconnecting")
 			case "register":
-				// asking for username, phonenumber and password for registering the user
-				reader := bufio.NewReader(os.Stdin)
-
-				fmt.Print("Enter Username: ")
-				username, err := reader.ReadString('\n')
-				if err != nil {
-					fmt.Printf("invalid username")
-					return
-				}
-
-				fmt.Print("Enter Phonenumber: ")
-				phonenumber, err := reader.ReadString('\n')
-				if err != nil {
-					fmt.Printf("invalid phonenumber")
-					return
-				}
-
-				fmt.Print("Enter Password: ")
-				password, err := reader.ReadString('\n')
-				if err != nil {
-					fmt.Printf("invalid password")
-					return
-				}
+				phonenumber := f.Value
 
 				// requesting the server to send OTP to given phonenumber
-				otpRequest, err := json.Marshal(struct {
+				otpRequestData, err := json.Marshal(struct {
 					Phonenumber string `json:"phonenumber"`
 				}{
-					Phonenumber: phonenumber,
+					Phonenumber: phonenumber.String(),
 				})
 				if err != nil {
 					fmt.Printf("Error sending request: %v", err)
 					return
 				}
 
-				// creating http post request
-				request, err := http.NewRequest("POST", "http://localhost:8080/api/v1/auth/otp/send", bytes.NewBuffer(otpRequest))
+				// creating http post request for sending otp to user phonenumber
+				otpRequest, err := createRequest("POST", "http://localhost:8080/api/v1/auth/otp/send", otpRequestData)
 				if err != nil {
 					fmt.Printf("Error creating request: %v", err)
 					return
 				}
 
-				// setting request header
-				request.Header.Set("Content-Type", "application/json")
-
-				// creating http client
-				httpClient := &http.Client{}
-
 				// sending the request
-				response, err := httpClient.Do(request)
+				response, err := httpClient.Do(otpRequest)
 				if err != nil {
 					fmt.Printf("Error sending the request: %v", err)
 					return
+				}
+
+				// checking if we can use the same otp
+				if response.StatusCode != http.StatusOK {
+					// create a new function that takes user input for
+					// username, password and otp
 				}
 				response.Body.Close()
 
 				// checking response status
 				// if response is 200 then asking user to enter the otp recieved and sending the registration request
 				if response.StatusCode == http.StatusOK {
+					// asking for username, password and OTP for registering the user
+					reader := bufio.NewReader(os.Stdin)
+
+					fmt.Print("Enter Username: ")
+					username, err := reader.ReadString('\n')
+					if err != nil {
+						fmt.Printf("invalid username")
+						return
+					}
+					username = strings.TrimSuffix(username, "\r\n")
+					fmt.Println(username[0])
+					fmt.Println(username[len(username)-1])
+
+					fmt.Print("Enter Password: ")
+					password, err := reader.ReadString('\n')
+					if err != nil {
+						fmt.Printf("invalid password")
+						return
+					}
+					password = strings.TrimSuffix(password, "\r\n")
+
 					fmt.Print("Enter OTP send to your phonenumber: ")
 					otp, err := reader.ReadString('\n')
 					if err != nil {
 						fmt.Printf("Error reading otp input: %v", err)
 						return
 					}
+					otp = strings.TrimSuffix(otp, "\r\n")
 
 					// marshalling the registration information and sending registration request to server
 					registrationInformation, err := json.Marshal(struct {
@@ -110,7 +124,7 @@ var userCmd = &cobra.Command{
 						OTP         string `json:"otp"`
 					}{
 						Username:    username,
-						Phonenumber: phonenumber,
+						Phonenumber: phonenumber.String(),
 						Password:    password,
 						OTP:         otp,
 					})
@@ -119,21 +133,29 @@ var userCmd = &cobra.Command{
 						return
 					}
 
-					// sending registration request to server
-					request.URL.Path = "/api/v1/user/register"
-					request.Body = io.NopCloser(bytes.NewBuffer(registrationInformation))
+					// creating registration request
+					registrationRequest, err := createRequest("POST", "http://localhost:8080/api/v1/auth/register", registrationInformation)
+					if err != nil {
+						log.Printf("Error creating registration request: %v", err)
+						return
+					}
 
-					response, err = httpClient.Do(request)
+					response, err = httpClient.Do(registrationRequest)
 					if err != nil {
 						fmt.Printf("Error sending registration request: %v", err)
 						return
 					}
-					response.Body.Close()
 
 					// checking response status
 					if response.StatusCode == http.StatusCreated {
 						fmt.Println("Registration Successful")
-						utility.ClearConsole()
+					} else if response.StatusCode > 399 && response.StatusCode < 500 {
+						log.Printf("Error %d", response.StatusCode)
+					} else {
+						fmt.Println(response.StatusCode)
+						params := utility.DecodeResponseBody(response.Body, &utility.ErrorResponse{}).(*utility.ErrorResponse)
+						log.Printf("Error: %s", params.Error)
+						response.Body.Close()
 					}
 				}
 			}
@@ -155,7 +177,7 @@ func init() {
 	// userCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 	userCmd.Flags().StringP("connect", "c", "", "This command will connect you to the server")
 	userCmd.Flags().StringP("disconnect", "d", "", "This command will diconnect you from the sever")
-	userCmd.Flags().StringP("register", "r", "", "This command will help you register for service")
+	userCmd.Flags().StringP("register", "r", "", "This command will help you register for service.\nIt takes username, phonenumber and password as input(space separated)")
 	userCmd.Flags().StringP("search", "s", "", "This command will help you search for a user")
 	userCmd.Flags().Bool("remove", false, "This command will help you delete your account")
 	userCmd.Flags().StringP("update", "u", "", "This command helps you to update your credentials but you have to provide what you want to update like username, password, phonenumber")
