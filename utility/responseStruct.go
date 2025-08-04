68d42db790c7a5ceabe4720656069c7aee9f6ff0
harshvardhan singh <harshvardhansingh458@protonmail.com>
2025-08-04 18:29:39 +0530

    login response decoder added

--- utility/responseStruct.go
+++ utility/responseStruct.go
@@ -15,6 +15,30 @@ type UpdateUsernameResponse struct {
 	AccessToken string `json:"access_token"`
 }
 
+type SearchUserResponse struct {
+	Username    string `json:"username"`
+	CreatedAt   string `json:"created_at"`
+	AccessToken string `json:"access_token"`
+}
+
+type oneToOneMessages struct {
+	Sender           string
+	Messages         string
+	TotalNewMessages int64
+}
+
+type groupMessages struct {
+	GroupName        string
+	Messages         string
+	TotalNewMessages int64
+}
+
+type LatestMessages struct {
+	OneToOneMessages []oneToOneMessages `json:"oneToOneMessages"`
+	GroupMessages    []groupMessages    `json:"groupMessages"`
+	AccessToken      string             `json:"access_token"`
+}
+
 func DecodeResponseBody(body io.Reader, responseStruct any) any {
 	decoder := json.NewDecoder(body)
 	err := decoder.Decode(responseStruct)
