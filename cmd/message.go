ffa536356ec5bf7e73c0a615143aca19bd58f9b5
harshvardhan singh <harshvardhansingh458@protonmail.com>
2025-08-13 02:59:50 +0530

    started working on message command

--- cmd/message.go
+++ cmd/message.go
@@ -1,40 +1,31 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 )
 
 // messageCmd represents the message command
 var messageCmd = &cobra.Command{
 	Use:   "message",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Manage specific message by index",
+	Long: `The 'message' command allows you to interact with a specific
+			message using its unique numerical index.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("message called")
+		cmd.Flags().Visit(func(f *pflag.Flag) {
+			// process the flags for message command
+		})
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(messageCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// messageCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// messageCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// adding local flags to message command
+	messageCmd.Flags().String("new", "", "input: <new_message>")
+	messageCmd.Flags().Int("edit", -1, "input: <message_index> <edited_message>")
+	messageCmd.Flags().Int("delete", -1, "input: <message_index>")
 }
