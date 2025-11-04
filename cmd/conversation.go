0103c0b9ea7534414b5dacaa8435005c13367493
harshvardhan singh <harshvardhansingh458@protonmail.com>
2025-11-04 15:33:00 +0530

    refactor and messages subcommand of conversation command is in the same file now

--- cmd/conversation.go
+++ cmd/conversation.go
@@ -6,6 +6,7 @@ package cmd
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -18,6 +19,51 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 )
+
+// this function provides oneToOneConversations Map
+func getOneToOneConversationMap() map[int]utility.OneToOneConversation {
+	oneToOneConversationsMap := make(map[int]utility.OneToOneConversation)
+	jsonData, err := os.ReadFile("one_to_one.json")
+	if err != nil {
+		log.Printf("error reading from one to one conversation json file")
+		return nil
+	}
+
+	if err = json.Unmarshal(jsonData, &oneToOneConversationsMap); err != nil {
+		log.Printf("error unmarshalling one to one conversation json data: %v", err)
+		return nil
+	}
+
+	return oneToOneConversationsMap
+}
+
+// this function will update the auth file when recieved empty response
+func updateAuthFileForEmptyResponse(responseBody io.Reader) {
+	emptyResponse := utility.DecodeResponseBody(responseBody, &utility.EmptyResponse{}).(*utility.EmptyResponse)
+	if emptyResponse != nil {
+		if len(emptyResponse.AccessToken) > 0 {
+			if err := os.WriteFile("token.auth", []byte(emptyResponse.AccessToken), 0770); err != nil {
+				log.Printf("error updating auth file: %v", err)
+			}
+		}
+	}
+}
+
+// this function provided messages map from json file
+func getMessagesMap(fileName string) map[int]utility.Message {
+	messagesMap := make(map[int]utility.Message)
+	messagesJsonData, err := os.ReadFile(fmt.Sprintf("%s.json", fileName))
+	if err != nil {
+		log.Printf("error reading from messages file: %v", err)
+		return nil
+	}
+	if err = json.Unmarshal(messagesJsonData, &messagesMap); err != nil {
+		log.Printf("error unmarshalling messages json data: %v", err)
+		return nil
+	}
+
+	return messagesMap
+}
 
 var conversationIndex int
 
@@ -147,30 +193,10 @@ var conversationCmd = &cobra.Command{
 				}
 
 				// counting the number of receiver ids in one_to_one.conv
-				oneToOneFileContents, err := os.ReadFile("one_to_one.json")
-				if err != nil {
-					log.Printf("error fetching conversation: %v", err)
-					return
-				}
-
-				oneToOneConversationsMap := make(map[int]utility.OneToOneConversation)
-				if err = json.Unmarshal(oneToOneFileContents, &oneToOneConversationsMap); err != nil {
-					log.Printf("error unmarshalling one to one conversations json: %v", err)
-					return
-				}
+				oneToOneConversationsMap := getOneToOneConversationMap()
 
 				// counting the number of group ids in group.conv
-				groupFileContents, err := os.ReadFile("group.json")
-				if err != nil {
-					log.Printf("error fetching conversation: %v", err)
-					return
-				}
-
-				groupConversationsMap := make(map[int]utility.GroupConversation)
-				if err = json.Unmarshal(groupFileContents, &groupConversationsMap); err != nil {
-					log.Printf("error unmarshalling group conversation json: %v", err)
-					return
-				}
+				groupConversationsMap := getGroupsMapFromJsonFile()
 
 				// checking if receiver id exist in one_to_one or group conversation file
 				if index-1 < len(oneToOneConversationsMap) {
@@ -346,18 +372,7 @@ var conversationCmd = &cobra.Command{
 				}
 
 				// checking if this index exist in one_to_one conversation json file
-				oneToOneConversations, err := os.ReadFile("one_to_one.json")
-				if err != nil {
-					log.Printf("error reading one_to_one conversation json file: %v", err)
-					return
-				}
-
-				// unmarshalling into map
-				oneToOneConversationsMap := make(map[int]utility.OneToOneConversation)
-				if err = json.Unmarshal(oneToOneConversations, &oneToOneConversationsMap); err != nil {
-					log.Printf("error unmarshalling one to one conversation json: %v", err)
-					return
-				}
+				oneToOneConversationsMap := getOneToOneConversationMap()
 
 				// checking if the key exist in the map
 				value, ok := oneToOneConversationsMap[index-1]
@@ -397,12 +412,7 @@ var conversationCmd = &cobra.Command{
 				// processing response based on status codes
 				switch response.StatusCode {
 				case http.StatusOK:
-					emptyResponse := utility.DecodeResponseBody(response.Body, &utility.EmptyResponse{}).(*utility.EmptyResponse)
-					if len(emptyResponse.AccessToken) > 0 {
-						if err = os.WriteFile("token.auth", []byte(emptyResponse.AccessToken), 0770); err != nil {
-							log.Printf("error writing auth token to auth file: %v", err)
-						}
-					}
+					updateAuthFileForEmptyResponse(response.Body)
 				case http.StatusBadRequest:
 					errorResponse := utility.DecodeResponseBody(response.Body, &utility.ErrorResponse{}).(*utility.ErrorResponse)
 					if errorResponse != nil {
@@ -410,6 +420,420 @@ var conversationCmd = &cobra.Command{
 					}
 				case http.StatusInternalServerError:
 					log.Print("server error")
+				}
+			}
+		})
+	},
+}
+
+// messageCmd represents the message command
+var messageCmd = &cobra.Command{
+	Use:   "message",
+	Short: "Manage specific message by index",
+	Long: `The 'message' command allows you to interact with a specific
+			message using its unique numerical index.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Flags().Visit(func(f *pflag.Flag) {
+			// process the flags for message command
+			flag := f.Name
+
+			// creating http client to send requests to server
+			httpClient := http.Client{}
+
+			// loading authentication token
+			authToken, err := os.ReadFile("token.auth")
+			if err != nil {
+				log.Printf("error reading auth token: %v", err)
+				return
+			}
+
+			// checking if the conversation index is valid or not
+			if conversationIndex <= 0 {
+				log.Print("invalid conversation index")
+				return
+			}
+
+			switch strings.ToLower(flag) {
+			case "new":
+				// checking if the conversation index is in one to one conversation json file
+				oneToOneConversationsMap := getOneToOneConversationMap()
+
+				if conversationIndex-1 < len(oneToOneConversationsMap) {
+					// creating new message request
+					message := f.Value.String()
+					requestBody, err := json.Marshal(struct {
+						Description string `json:"description"`
+						ReceiverID  string `json:"receiver_id"`
+						GroupID     string `json:"group_id"`
+					}{
+						Description: message,
+						ReceiverID:  oneToOneConversationsMap[conversationIndex-1].ReceiverID.String(),
+						GroupID:     "",
+					})
+					if err != nil {
+						log.Printf("error creating request body for new message request: %v", err)
+						return
+					}
+
+					// creating request
+					newMessageRequest, err := CreateRequest("POST", "http://localhost:8080/api/v1/message/create", requestBody)
+					if err != nil {
+						log.Printf("error creating new message request please try again: %v", err)
+						return
+					}
+
+					// sending request
+					newMessageRequest.Header.Add("authorization", fmt.Sprintf("bearer %s", authToken))
+					response, err := httpClient.Do(newMessageRequest)
+					if err != nil {
+						log.Printf("error sending request please try again: %v", err)
+						return
+					}
+
+					// parsing response
+					switch response.StatusCode {
+					case http.StatusCreated:
+						log.Printf("message sent!")
+						updateAuthFileForEmptyResponse(response.Body)
+						return
+					case http.StatusInternalServerError:
+						log.Printf("server error")
+					case http.StatusBadRequest:
+						fallthrough
+					case http.StatusNotAcceptable:
+						errorResponse := utility.DecodeResponseBody(response.Body, &utility.ErrorResponse{}).(*utility.ErrorResponse)
+						if errorResponse != nil {
+							log.Print(errorResponse.Error)
+						}
+					}
+				}
+
+				// checking if conversation index is in group conversations json file
+				groupConversationsMap := getGroupsMapFromJsonFile()
+
+				if conversationIndex-1 < len(groupConversationsMap) {
+					// creating new group message
+					message := f.Value.String()
+
+					// creating request body
+					requestBody, err := json.Marshal(struct {
+						Description string `json:"description"`
+						ReceiverID  string `json:"receiver_id"`
+						GroupID     string `json:"group_id"`
+					}{
+						Description: message,
+						ReceiverID:  "",
+						GroupID:     groupConversationsMap[conversationIndex-1].GroupID.UUID.String(),
+					})
+					if err != nil {
+						log.Printf("error creating new group message request body: %v", err)
+						return
+					}
+
+					// creating request
+					request, err := CreateRequest("POST", "http://localhost:8080/api/v1/message/create", requestBody)
+					if err != nil {
+						log.Printf("error creating request: %v", err)
+						return
+					}
+
+					// sending request
+					request.Header.Add("authorization", fmt.Sprintf("bearer %s", authToken))
+					response, err := httpClient.Do(request)
+					if err != nil {
+						log.Printf("error sending request: %v", err)
+						return
+					}
+
+					// parsing response
+					switch response.StatusCode {
+					case http.StatusCreated:
+						log.Print("message sent!")
+						updateAuthFileForEmptyResponse(response.Body)
+						return
+					case http.StatusInternalServerError:
+						log.Printf("server error")
+					case http.StatusBadRequest:
+						fallthrough
+					case http.StatusNotAcceptable:
+						errorResponse := utility.DecodeResponseBody(response.Body, &utility.ErrorResponse{}).(*utility.ErrorResponse)
+						if errorResponse != nil {
+							log.Print(errorResponse.Error)
+						}
+					default:
+						log.Print("invalid response")
+					}
+				}
+			case "edit":
+				// getting message index to edit
+				messageIndexString := f.Value.String()
+				messageIndex, err := strconv.Atoi(messageIndexString)
+				if err != nil {
+					log.Printf("error converting message index from string to integer: %v", err)
+					return
+				}
+
+				// getting the new edited message from args
+				editedMessage := args[0]
+
+				if conversationIndex <= 0 {
+					log.Printf("invalid conversation index")
+					return
+				}
+
+				// checking if the conversation index is a valid one_to_one conversation index or group conversation index
+				oneToOneConversationMap := getOneToOneConversationMap()
+				if conversationIndex-1 < len(oneToOneConversationMap) {
+					// checking if the provided message index is valid or not
+					// if its valid then sending the request for editing the message
+					messagesMap := getMessagesMap(oneToOneConversationMap[conversationIndex-1].ReceiverID.String())
+					if messageIndex <= 0 || messageIndex-1 > len(messagesMap) {
+						log.Print("invalid message index")
+						return
+					}
+
+					// creating request body
+					requestBody, err := json.Marshal(struct {
+						ID          uuid.UUID `json:"id"`
+						Description string    `json:"description"`
+						ReceiverID  uuid.UUID `json:"receiver_id"`
+						GroupID     uuid.UUID `json:"group_id"`
+					}{
+						ID:          messagesMap[messageIndex-1].ID,
+						Description: editedMessage,
+						ReceiverID:  messagesMap[messageIndex-1].RecieverID.UUID,
+						GroupID:     uuid.Nil,
+					})
+					if err != nil {
+						log.Printf("error creating request body: %v", err)
+						return
+					}
+
+					// creating request
+					request, err := CreateRequest("PUT", "http://localhost:8080/api/v1/message/update", requestBody)
+					if err != nil {
+						log.Printf("error creating update message request: %v", err)
+						return
+					}
+
+					// sending request
+					request.Header.Add("authorization", string(authToken))
+					response, err := httpClient.Do(request)
+					if err != nil {
+						log.Printf("error sending update message request: %v", err)
+						return
+					}
+
+					// parsing response
+					switch response.StatusCode {
+					case http.StatusOK:
+						log.Print("message updated")
+						updateAuthFileForEmptyResponse(response.Body)
+						return
+					case http.StatusInternalServerError:
+						log.Printf("server error")
+					case http.StatusNotAcceptable:
+						fallthrough
+					case http.StatusBadRequest:
+						errorResponse := utility.DecodeResponseBody(response.Body, &utility.ErrorResponse{}).(*utility.ErrorResponse)
+						if errorResponse != nil {
+							log.Print(errorResponse.Error)
+						}
+					}
+				}
+
+				groupConversationsMap := getGroupsMapFromJsonFile()
+				if conversationIndex-1 < len(groupConversationsMap) {
+					// checking if the message index is a valid group message index
+					messagesMap := getMessagesMap(groupConversationsMap[conversationIndex-1].GroupID.UUID.String())
+					if messageIndex <= 0 || messageIndex-1 > len(messagesMap) {
+						log.Print("invalid message index")
+						return
+					}
+
+					// creating request body
+					requestBody, err := json.Marshal(struct {
+						ID          uuid.UUID `json:"id"`
+						Description string    `json:"description"`
+						ReceiverID  uuid.UUID `json:"receiver_id"`
+						GroupID     uuid.UUID `json:"group_id"`
+					}{
+						ID:          messagesMap[messageIndex-1].ID,
+						Description: editedMessage,
+						ReceiverID:  uuid.Nil,
+						GroupID:     messagesMap[messageIndex-1].GroupID.UUID,
+					})
+					if err != nil {
+						log.Printf("error creating group message update request body: %v", err)
+						return
+					}
+
+					// creating request
+					request, err := CreateRequest("PUT", "http://localhost:8080/api/v1/message/update", requestBody)
+					if err != nil {
+						log.Printf("error creating update message request: %v", err)
+						return
+					}
+
+					// sending request
+					request.Header.Add("authorization", fmt.Sprintf("bearer %s", authToken))
+					response, err := httpClient.Do(request)
+					if err != nil {
+						log.Printf("error sending update message request: %v", err)
+						return
+					}
+
+					// parsing response
+					switch response.StatusCode {
+					case http.StatusOK:
+						log.Print("message updated!")
+						updateAuthFileForEmptyResponse(response.Body)
+						return
+					case http.StatusInternalServerError:
+						log.Print("server error")
+					case http.StatusBadRequest:
+						fallthrough
+					case http.StatusNotAcceptable:
+						errorResponse := utility.DecodeResponseBody(response.Body, &utility.ErrorResponse{}).(*utility.ErrorResponse)
+						if errorResponse != nil {
+							log.Print(errorResponse.Error)
+						}
+					default:
+						log.Print("invalid response")
+					}
+				}
+
+				log.Print("invalid conversation index")
+			case "delete":
+				messageIndexString := f.Value.String()
+				messageIndex, err := strconv.Atoi(messageIndexString)
+				if err != nil {
+					log.Printf("error parsing message Index into integer: %v", err)
+					return
+				}
+				if messageIndex <= 0 || conversationIndex <= 0 {
+					log.Printf("invalid message index or conversation index")
+					return
+				}
+
+				// checking if message belongs to one to one conversation
+				oneToOneConversationsMap := getOneToOneConversationMap()
+				if messageIndex-1 < len(oneToOneConversationsMap) {
+					// reading from messages json file
+					messagesMap := getMessagesMap(oneToOneConversationsMap[conversationIndex-1].ReceiverID.String())
+					if messageIndex-1 > len(messagesMap) {
+						log.Printf("invalid message index")
+						return
+					}
+
+					// creating request body
+					requestBody, err := json.Marshal(struct {
+						ID      uuid.UUID `json:"id"`
+						GroupID uuid.UUID `json:"group_id"`
+					}{
+						ID:      messagesMap[messageIndex-1].ID,
+						GroupID: uuid.Nil,
+					})
+					if err != nil {
+						log.Printf("error creating delete message request body: %v", err)
+						return
+					}
+
+					// creating request
+					request, err := CreateRequest("DELETE", "http://localhost:8080/api/v1/message/delete", requestBody)
+					if err != nil {
+						log.Printf("error creating message delete request: %v", err)
+						return
+					}
+
+					// sending request
+					request.Header.Add("authorization", fmt.Sprintf("bearer %s", authToken))
+					response, err := httpClient.Do(request)
+					if err != nil {
+						log.Printf("error sending request: %v", err)
+						return
+					}
+
+					// parsing response
+					switch response.StatusCode {
+					case http.StatusOK:
+						log.Print("message deleted!")
+						updateAuthFileForEmptyResponse(response.Body)
+						return
+					case http.StatusInternalServerError:
+						log.Print("server error")
+					case http.StatusBadRequest:
+						fallthrough
+					case http.StatusNotAcceptable:
+						fallthrough
+					case http.StatusNotFound:
+						errorResponse := utility.DecodeResponseBody(response.Body, &utility.ErrorResponse{}).(*utility.ErrorResponse)
+						if errorResponse != nil {
+							log.Print(errorResponse.Error)
+						}
+					default:
+						log.Print("invalid response")
+					}
+				}
+
+				// checking if message belongs to group conversations
+				groupConversationsMap := getGroupsMapFromJsonFile()
+				if conversationIndex-1 < len(groupConversationsMap) {
+					// checking if the messageIndex is valid or not
+					messagesMap := getMessagesMap(groupConversationsMap[conversationIndex-1].GroupID.UUID.String())
+					if messageIndex-1 > len(messagesMap) {
+						log.Print("invalid message index")
+						return
+					}
+
+					// creating request body
+					requestBody, err := json.Marshal(struct {
+						ID      uuid.UUID `json:"id"`
+						GroupID uuid.UUID `json:"group_id"`
+					}{
+						ID:      messagesMap[messageIndex-1].ID,
+						GroupID: messagesMap[messageIndex-1].GroupID.UUID,
+					})
+					if err != nil {
+						log.Printf("error creating request body: %v", err)
+						return
+					}
+
+					// creating request
+					request, err := CreateRequest("DELETE", "http://localhost:8080/api/v1/message/delete", requestBody)
+					if err != nil {
+						log.Printf("error creating delete message request: %v", err)
+						return
+					}
+
+					// sending request
+					request.Header.Add("authorization", fmt.Sprintf("bearer %s", authToken))
+					response, err := httpClient.Do(request)
+					if err != nil {
+						log.Printf("error sending request: %v", err)
+						return
+					}
+
+					// parsing response
+					switch response.StatusCode {
+					case http.StatusOK:
+						log.Print("message deleted!")
+						updateAuthFileForEmptyResponse(response.Body)
+						return
+					case http.StatusInternalServerError:
+						log.Print("server error")
+					case http.StatusBadRequest:
+						fallthrough
+					case http.StatusNotAcceptable:
+						fallthrough
+					case http.StatusNotFound:
+						errorResponse := utility.DecodeResponseBody(response.Body, &utility.ErrorResponse{}).(*utility.ErrorResponse)
+						if errorResponse != nil {
+							log.Print(errorResponse.Error)
+						}
+					default:
+						log.Print("invalid response")
+					}
 				}
 			}
 		})
@@ -425,4 +849,9 @@ func init() {
 	conversationCmd.Flags().Int("open", -1, "input: <conversation_index>. provides all the messages of a conversation")
 	conversationCmd.Flags().Int("delete", -1, "input: <conversation_index>. deletes the entire conversation")
 	conversationCmd.Flags().IntVar(&conversationIndex, "index", -1, "input: <conversation_index>. this will be used along with message command and its flags")
+
+	// adding local flags to message command
+	messageCmd.Flags().String("new", "", "input: <new_message>")
+	messageCmd.Flags().Int("edit", -1, "input: <message_index> <edited_message>")
+	messageCmd.Flags().Int("delete", -1, "input: <message_index>")
 }
